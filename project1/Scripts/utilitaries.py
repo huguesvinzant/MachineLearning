07b7dbfdc27fad5c969799d76ea2e8cee09f51f2
abisi <axel.bisi@epfl.ch>
2018-10-15 00:10:28 +0200

    Code cleaning
    Cleaned first half of the data_exploration notebook.
    Made a function nan_proportion (utilitary.py) from portion of code.
    Renamed some variables for clarity (e.g raw_features vs features).  Fragmented code portions. Added titles, comments, function descriptions.

--- project1/Scripts/utilitaries.py
+++ project1/Scripts/utilitaries.py
@@ -31,12 +31,30 @@ def compute_stoch_gradient(y, tx, w):
     grad = -tx.T.dot(err) / len(err)
     return grad, err
 
-def remove_meaningless_data(tx):
-    """ Remove meaningless -999 values from training dataset."""
+def meaningless_to_nan(tx):
+    """Remove meaningless -999 values (as explained in the guidelines) 
+        from training dataset by converting them into NaN."""
     tx[tx == -999] = np.NaN
     return tx
 
+def nan_proportion(tx):
+    """Calculate and display the proportion [0,1] of NaN per feature (column)."""
+    n_samples = tx.shape[0]
+    n_features = tx.shape[1]
+    #init. matrix that will contain proportions
+    prop_nan = np.zeros((2,30))
+    
+    for feature in range(n_features):
+        n_nan = 0
+        for sample in range(n_samples):
+            if np.isnan(tx[sample,feature]):
+                n_nan += 1
+                prop_nan[0,feature] = feature
+                prop_nan[1,feature] = n_nan/n_samples
+    return prop_nan
+
 def standardize(x):
+    """Standardize the data feature-wise ignoring NaN entries."""
     mean =  np.nanmean(x,0) #dim D | or center the data directly : centered_data = x - np.mean(x,axis=0)
     std = np.nanstd(x,0) 
     return (x - mean) / std
