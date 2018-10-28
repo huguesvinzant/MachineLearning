f128e8a477e7648c02d2178790b943df84cabc7e
hvinzant <hugues.vinzant@epfl.ch>
2018-10-28 14:42:07 +0100

    Change so we don't estimate column 0 anymore

--- project1/Scripts/run.py
+++ project1/Scripts/run.py
@@ -55,46 +55,31 @@ std_data1_te = standardize_test(clean_data1_te, mean1, std1)
 std_data2, mean2, std2 = standardize_train(clean_data2)
 std_data2_te = standardize_test(clean_data2_te, mean2, std2)
 
-#Column 0 estimation
-
-print('Data processing...: estimation of the Nan values in column 0')
-estimated_data0, weights_train0,samples0 = column_estimation_train(std_data0)
-print('Data processing...:', samples0, 'NaN lines found in "0 jet"') 
-estimated_data0_te = column_estimation_test(std_data0_te, weights_train0)
-
-estimated_data1, weights_train1,samples1 = column_estimation_train(std_data1)
-print('Data processing...:', samples1, 'NaN lines found in "1 jet"') 
-estimated_data1_te = column_estimation_test(std_data1_te, weights_train1)
-
-estimated_data2, weights_train2,samples2 = column_estimation_train(std_data2)
-print('Data processing...:', samples2, 'NaN lines found in "2 or more jets"') 
-estimated_data2_te = column_estimation_test(std_data2_te, weights_train2)
-
 
         # ----  RIDGE REGRESSION  ----
 
 #Define parameters
-degrees = [9,12]
-lambdas = np.logspace(-4, -3, 2)
+degrees = [9,10,11,12,13]
+lambdas = np.logspace(-1, -5, 5)
 k_fold = 10
 seed = 23
 
 #Find best parameters (cross validation)
 print('Ridge regression... : Finding best parameters with cross validation...')
-best_degree0, best_lambda0, best_score0, _ = find_best_parameters_general(labels0, estimated_data0, k_fold, seed, lambdas=lambdas, degrees=degrees)
+best_degree0, best_lambda0, best_score0, _ = find_best_parameters_general(labels0, std_data0, k_fold, seed, lambdas=lambdas, degrees=degrees)
 print('Ridge regression... : 0 jet:', 'Best degree:', best_degree0, 'Best lambda:', best_lambda0, 'Best score:', best_score0)
 
-best_degree1, best_lambda1,  best_score1, _ = find_best_parameters_general(labels1, estimated_data1, k_fold, seed, lambdas=lambdas, degrees=degrees)
+best_degree1, best_lambda1,  best_score1, _ = find_best_parameters_general(labels1, std_data1, k_fold, seed, lambdas=lambdas, degrees=degrees)
 print('Ridge regression... : 1 jet:', 'Best degree:', best_degree1, 'Best lambda:', best_lambda1, 'Best score:', best_score1)
 
-best_degree2, best_lambda2, best_score2, _ = find_best_parameters_general(labels2, estimated_data2, k_fold, seed, lambdas=lambdas, degrees=degrees)
+best_degree2, best_lambda2, best_score2, _ = find_best_parameters_general(labels2, std_data2, k_fold, seed, lambdas=lambdas, degrees=degrees)
 print('Ridge regression... : 2 or more jets:', 'Best degree:', best_degree2, 'Best lambda:', best_lambda2, 'Best score:', best_score2)
 
 #Prediction
 
-y_pred0 = make_predictions(estimated_data0, labels0, estimated_data0_te, best_lambda0, best_degree0)
-y_pred1 = make_predictions(estimated_data1, labels1, estimated_data1_te, best_lambda1, best_degree1)
-y_pred2 = make_predictions(estimated_data2, labels2, estimated_data2_te, best_lambda2, best_degree2)
+y_pred0 = make_predictions(std_data0, labels0, std_data0_te, best_lambda0, best_degree0)
+y_pred1 = make_predictions(std_data1, labels1, std_data1_te, best_lambda1, best_degree1)
+y_pred2 = make_predictions(std_data2, labels2, std_data2_te, best_lambda2, best_degree2)
 
 #Submission
 
@@ -115,29 +100,29 @@ labels2[labels2 == -1] = 0
 
 #Define parameters
 
-degrees = [1,3]
-lambdas = np.logspace(-1, -3, 2)
+degrees = range(5)
+lambdas = np.logspace(-1, -5, 5)
 k_fold = 10
 gamma = 1e-5
 
 #Find best parameters (cross validation)
 
 print('Logistic regression...: Finding best parameters with cross validation...')
-best_degree0, best_lambda0, best_score0, _ = find_best_parameters_general(labels0, estimated_data0, k_fold, seed, lambdas=lambdas, degrees=degrees, gamma=gamma)
+best_degree0, best_lambda0, best_score0, _ = find_best_parameters_general(labels0, std_data0, k_fold, seed, lambdas=lambdas, degrees=degrees, gamma=gamma)
 print('Logistic regression...: 0 jet:', 'Best degree:', best_degree0, 'Best lambda:', best_lambda0, 'Best score:', best_score0)
 
-best_degree1, best_lambda1, best_score1, _ = find_best_parameters_general(labels1, estimated_data1, k_fold, seed, lambdas=lambdas, gamma=gamma, degrees=degrees)
+best_degree1, best_lambda1, best_score1, _ = find_best_parameters_general(labels1, std_data1, k_fold, seed, lambdas=lambdas, gamma=gamma, degrees=degrees)
 print('Logistic regression...: 1 jet:', 'Best degree:', best_degree1, 'Best lambda:', best_lambda1, 'Best score:', best_score1)
 
-best_degree2, best_lambda2, best_score2, _ = find_best_parameters_general(labels2, estimated_data2, k_fold, seed, lambdas=lambdas, gamma=gamma, degrees=degrees)
+best_degree2, best_lambda2, best_score2, _ = find_best_parameters_general(labels2, std_data2, k_fold, seed, lambdas=lambdas, gamma=gamma, degrees=degrees)
 print('Logistic regression...: 2 or more jets:', 'Best degree:', best_degree2, 'Best lambda:', best_lambda2, 'Best score:', best_score2)
 
 #Prediction 
 
 max_iters = 1000
-y_pred0 = make_predictions_log(estimated_data0, labels0, estimated_data0_te, best_lambda0, best_degree0, max_iters, gamma)
-y_pred1 = make_predictions_log(estimated_data1, labels1, estimated_data1_te, best_lambda1, best_degree1, max_iters, gamma)
-y_pred2 = make_predictions_log(estimated_data2, labels2, estimated_data2_te, best_lambda2, best_degree2, max_iters, gamma)
+y_pred0 = make_predictions_log(std_data0, labels0, std_data0_te, best_lambda0, best_degree0, max_iters, gamma)
+y_pred1 = make_predictions_log(std_data1, labels1, std_data1_te, best_lambda1, best_degree1, max_iters, gamma)
+y_pred2 = make_predictions_log(std_data2, labels2, std_data2_te, best_lambda2, best_degree2, max_iters, gamma)
 
 #Submission 
 
