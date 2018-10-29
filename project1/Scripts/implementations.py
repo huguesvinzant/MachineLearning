46be2267db63c2f69440677825af48211621367c
hvinzant <hugues.vinzant@epfl.ch>
2018-10-29 16:06:02 +0100

    batch_iter

--- project1/Scripts/implementations.py
+++ project1/Scripts/implementations.py
@@ -137,6 +137,24 @@ def compute_loss(y, tx, w):
     e = y - y_pred
     
     return  1/2*np.mean(e**2)
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """Generate a minibatch iterator for a dataset."""
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
     
 
 def compute_loss_labels(y, tx, w):
